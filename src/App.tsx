7ef91bcba319196cec3cdb19337346ba97f0137f
Karmit Personal <karmit199@gmail.com>
2021-04-28 22:59:00 -0400

    [Feat] Added a Header.

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,12 @@ import './App.css';
 import { Homepage } from './pages/homepage/homepage';
 import { Route, Switch } from 'react-router-dom';
 import ShopPage from './pages/shop/ShopPage';
+import Header from './components/header/header';
 
 function App() {
     return (
         <div className = "App">
+            <Header/>
             <Switch>
                 <Route exact path = "/" component = {Homepage}/>
                 <Route exact path = "/shop" component = {ShopPage}/>
