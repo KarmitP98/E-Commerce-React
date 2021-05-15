008182b314c2ab4eff7abeff5a6fdd0511579bec
Karmit Personal <karmit199@gmail.com>
2021-05-15 17:33:48 -0400

    Implemented Stripe Integration

--- src/pages/checkout/CheckoutPage.jsx
+++ src/pages/checkout/CheckoutPage.jsx
@@ -1,41 +1,43 @@
 import React from 'react';
-import {connect} from 'react-redux';
-import {createStructuredSelector} from 'reselect';
+import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
 import CheckoutItem from '../../components/checkout-item/CheckoutItem';
-import {selectCartItems, selectCartTotal} from '../../redux/cart/cart.selectors';
+import { selectCartItems, selectCartTotal } from '../../redux/cart/cart.selectors';
 import './checkout.styles.scss';
+import StripeButton from '../../components/stripe-button/stripe-button.component';
 
-const CheckoutPage = ({cartItems, totalValue}) => {
-    return (
-        <div className="checkout-page">
-            <div className="checkout-header ">
-                <div className="header-block">
-                    <span>Product</span>
-                </div>
-                <div className="header-block">
-                    <span>Description</span>
-                </div>
-                <div className="header-block">
-                    <span>Quantity</span>
-                </div>
-                <div className="header-block">
-                    <span>Price</span>
-                </div>
-                <div className="header-block">
-                    <span>Remove</span>
-                </div>
-            </div>
-            {cartItems.map((cartItem) => (
-                <CheckoutItem key={cartItem.id} item={cartItem}/>
-            ))}
-            <div className="total">Total: ${totalValue}</div>
-        </div>
-    );
+const CheckoutPage = ({ cartItems, totalValue }) => {
+	return (
+		<div className='checkout-page'>
+			<div className='checkout-header '>
+				<div className='header-block'>
+					<span>Product</span>
+				</div>
+				<div className='header-block'>
+					<span>Description</span>
+				</div>
+				<div className='header-block'>
+					<span>Quantity</span>
+				</div>
+				<div className='header-block'>
+					<span>Price</span>
+				</div>
+				<div className='header-block'>
+					<span>Remove</span>
+				</div>
+			</div>
+			{cartItems.map((cartItem) => (
+				<CheckoutItem key={cartItem.id} item={cartItem} />
+			))}
+			<div className='total'>Total: ${totalValue}</div>
+			<StripeButton price={totalValue}/>
+		</div>
+	);
 };
 
 const mapStateToProps = createStructuredSelector({
-    cartItems: selectCartItems,
-    totalValue: selectCartTotal,
+	cartItems: selectCartItems,
+	totalValue: selectCartTotal,
 });
 
 const mapDispatchToProps = {};
